9376fce8f20edc8ecbcaa653bc858956d23443d4
nermeen_mohamed <mohamednermeen110@gmail.com>
2021-11-22 08:55:16 +0200

    ............

--- src/screens/Movies/MoviesComponents/GenreItem.tsx
+++ src/screens/Movies/MoviesComponents/GenreItem.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from 'react'
+import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import colors from '../../../assets/colors'
 import { IGenre } from '../../../Interfaces/movies_interfaces'
@@ -14,9 +14,8 @@ const GenreItem = (props: GenreItemProps) => {
     const { index, item, forMovieCard = false } = props
     const { name } = item
 
-    let calculatedMarginLeft = useMemo(() => (forMovieCard ? 0 : (index === 0 || index % 4 === 0) && !forMovieCard) ? scale(20) : scale(5), [])
     return (
-        <View style={[styles.container, { marginLeft: calculatedMarginLeft }]}>
+        <View style={[styles.container, { marginLeft: (forMovieCard ? 0 : (index === 0 || index % 4 === 0) && !forMovieCard) ? scale(20) : scale(5) }]}>
             <Text style={styles.genreName}>{name}</Text>
         </View>
     )
